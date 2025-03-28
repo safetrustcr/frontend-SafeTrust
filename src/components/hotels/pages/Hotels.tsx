caffe5efc87075af5ebdbd1dee095d496e45faa6
JoelVR17 <vermudo.com@gmail.com>
2025-03-28 17:57:39 -0600

    feat: add Hotels page to integrate HotelBookingContainer component

---
+++ src/components/hotels/pages/Hotels.tsx
@@ -0,0 +1,13 @@
+import HotelBookingContainer from "../overall/HotelBookingContainer";
+
+const Hotels = () => {
+  return (
+    <div className="flex min-h-screen py-10">
+      <div className="flex-1">
+        <HotelBookingContainer />
+      </div>
+    </div>
+  );
+};
+
+export default Hotels;
