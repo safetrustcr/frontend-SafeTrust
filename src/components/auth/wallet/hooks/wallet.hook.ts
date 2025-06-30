db06367c8b45dc78aafaab0ada6309a56993e335
Zintarh <kateberryd@gmail.com>
2025-06-30 14:26:45 +0100

    feat: add signing a transaction

--- src/components/auth/wallet/hooks/wallet.hook.ts
+++ src/components/auth/wallet/hooks/wallet.hook.ts
@@ -1,21 +1,57 @@
-import { ISupportedWallet } from "@creit.tech/stellar-wallets-kit";
+import {
+  ISupportedWallet,
+  WalletNetwork,
+} from "@creit.tech/stellar-wallets-kit";
 import { kit } from "../constants/wallet-kit.constant";
 import { useGlobalAuthenticationStore } from "@/core/store/data";
+import getPublicKey from "@stellar/freighter-api";
 import { useRouter } from "next/navigation";
+import { Account, TransactionBuilder, Operation, Networks } from "stellar-sdk";
 
 export const useWallet = () => {
   const router = useRouter();
   const { connectWalletStore, disconnectWalletStore } =
     useGlobalAuthenticationStore();
 
+  function buildChallengeTx(
+    userPublicKey: string,
+    authPublicKey: string
+  ): string {
+    const dummyAccount = new Account(authPublicKey, "0");
+
+    const tx = new TransactionBuilder(dummyAccount, {
+      fee: "100",
+      networkPassphrase: Networks.PUBLIC,
+    })
+      .addOperation(
+        Operation.manageData({
+          name: "auth",
+          value: `Sign in @ ${new Date().toISOString()}`,
+          source: userPublicKey,
+        })
+      )
+      .setTimeout(300)
+      .build();
+
+    return tx.toXDR();
+  }
+
   const connectWallet = async () => {
     await kit.openModal({
       modalTitle: "Connect Wallet",
       onWalletSelected: async (option: ISupportedWallet) => {
         kit.setWallet(option.id);
-
         const { address } = await kit.getAddress();
         const { name } = option;
+
+        const authPublicKey = (await getPublicKey.getAddress()).address;
+
+        const challengeXDR = buildChallengeTx(address, authPublicKey);
+
+        await kit.signTransaction(challengeXDR, {
+          address,
+          networkPassphrase: WalletNetwork.TESTNET,
+        });
 
         connectWalletStore(address, name);
       },
