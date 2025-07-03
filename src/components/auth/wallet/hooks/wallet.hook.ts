06a67bd3886a49d94f678d2dbace7ddb54c01b53
Zintarh <kateberryd@gmail.com>
2025-07-03 05:09:52 +0100

    feat: remove spending of gas after login

--- src/components/auth/wallet/hooks/wallet.hook.ts
+++ src/components/auth/wallet/hooks/wallet.hook.ts
@@ -4,37 +4,14 @@ import {
 } from "@creit.tech/stellar-wallets-kit";
 import { kit } from "../constants/wallet-kit.constant";
 import { useGlobalAuthenticationStore } from "@/core/store/data";
-import getPublicKey from "@stellar/freighter-api";
 import { useRouter } from "next/navigation";
-import { Account, TransactionBuilder, Operation, Networks } from "stellar-sdk";
+import { useState } from "react";
 
 export const useWallet = () => {
   const router = useRouter();
-  const { connectWalletStore, disconnectWalletStore } =
+  const { connectWalletStore, address, name, disconnectWalletStore } =
     useGlobalAuthenticationStore();
-
-  function buildChallengeTx(
-    userPublicKey: string,
-    authPublicKey: string
-  ): string {
-    const dummyAccount = new Account(authPublicKey, "0");
-
-    const tx = new TransactionBuilder(dummyAccount, {
-      fee: "100",
-      networkPassphrase: Networks.PUBLIC,
-    })
-      .addOperation(
-        Operation.manageData({
-          name: "auth",
-          value: `Sign in @ ${new Date().toISOString()}`,
-          source: userPublicKey,
-        })
-      )
-      .setTimeout(300)
-      .build();
-
-    return tx.toXDR();
-  }
+  const [error, setError] = useState<string | null>(null);
 
   const connectWallet = async () => {
     await kit.openModal({
@@ -43,16 +20,6 @@ export const useWallet = () => {
         kit.setWallet(option.id);
         const { address } = await kit.getAddress();
         const { name } = option;
-
-        const authPublicKey = (await getPublicKey.getAddress()).address;
-
-        const challengeXDR = buildChallengeTx(address, authPublicKey);
-
-        await kit.signTransaction(challengeXDR, {
-          address,
-          networkPassphrase: WalletNetwork.TESTNET,
-        });
-
         connectWalletStore(address, name);
       },
     });
