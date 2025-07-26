2de0224c0690ee78ef9229edf9e1e43ebc6ef48c
Benjtalkshow <chinedubenj@gmail.com>
2025-07-26 16:45:12 +0100

    merge main

--- app/test-page/page.tsx
+++ app/test-page/page.tsx
@@ -1,6 +1,8 @@
 import ApolloTestComponent from '@/components/ApolloTestComponent';
 import React from 'react';
-
+//////////////////////////////
+//This is a test page for harusa and apollo client
+/////////////////////////////
 const page = () => {
   return (
     <div>
