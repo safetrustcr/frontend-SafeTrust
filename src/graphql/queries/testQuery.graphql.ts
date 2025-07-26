f10db92f52c24b95620013bb28e87cd97407b8c6
Benjtalkshow <chinedubenj@gmail.com>
2025-07-26 16:34:58 +0100

    setup harusa with docker

--- src/graphql/queries/testQuery.graphql.ts
+++ src/graphql/queries/testQuery.graphql.ts
@@ -1,15 +1,40 @@
 import { gql } from '@apollo/client';
 
 export const GET_ESCROW_TRANSACTIONS = gql`
-  query GetEscrowTransactions {
-    escrow_transactions(limit: 10) {
+  query GetEscrowTransactions($limit: Int = 10) {
+    escrow_transactions(limit: $limit, order_by: { created_at: desc }) {
       id
       contract_id
       created_at
+      status
       escrow_transaction_users {
         id
         funding_status
+        user {
+          id
+          email
+          first_name
+          last_name
+        }
       }
+    }
+  }
+`;
+
+export const HEALTH_CHECK_QUERY = gql`
+  query HealthCheck {
+    __typename
+  }
+`;
+
+export const GET_USERS = gql`
+  query GetUsers($limit: Int = 10) {
+    users(limit: $limit, order_by: { created_at: desc }) {
+      id
+      email
+      first_name
+      last_name
+      created_at
     }
   }
 `;
